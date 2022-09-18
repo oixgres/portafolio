19bdacf62cc82e4d78fc668bc4e2c484472c4ddf
oixgres <a1252819@uabc.edu.mx>
2022-09-18 16:20:49 -0700

    Using axios and geolocation-db to create logs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Presentation from "../components/Presentation";
+import GetData from "../util/GetData";
 
 const Home = () => {
+    // console.log(GetData());
+    // useEffect(() => {
+    //     GetData();
+    // }, []);
+
     return (
         <div>
             <Navbar location={'home'}/>
             <Presentation/>
+            <GetData/>
         </div>
     );
 }
---
+++ src/util/GetData.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useEffect,useState } from "react";
+
+const GetData = () => {
+    const [data, setData] = useState(null);
+    const [time, setTime] = useState(null);
+
+    // get data from api
+    useEffect(() => {
+        // setTime(getTime())
+        axios.get('https://geolocation-db.com/json')
+            .then((response) => {
+                setData({
+                    time: getTime(),
+                    ip: response.data.IPv4,
+                    country: response.data.country_code,
+                    state: response.data.state,
+                    city: response.data.city,
+                    postal: response.data.postal,
+                    latitude: response.data.latitude,
+                    longitude: response.data.longitude,
+                    status:'success',
+                    reason: '-'
+                })
+            }).catch((error) => {
+                setData({
+                    time: getTime(),
+                    ip: '-',
+                    country: '-',
+                    state: '-',
+                    city: '-',
+                    postal: '-',
+                    latitude: '-',
+                    longitude: '-',
+                    status:'error',
+                    reason: error.code
+                });
+        })
+
+    }, []);
+
+    useEffect(() => {
+        if (data === null)
+            return;
+
+        console.log(data);
+
+    }, [data]);
+
+    const getTime = () => {
+        const date = new Date();
+        const yyyymmdd = date.getFullYear()*10000+(date.getMonth()+1)*100+date.getDate();
+        const hhmmss = date.getHours()*10000+date.getMinutes()*100+date.getSeconds();
+        return `${yyyymmdd}${hhmmss}`;
+    }
+
+    return (
+        <p>Hello world</p>
+    )
+}
+
+export default GetData;
